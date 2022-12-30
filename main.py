98bc77115c97af6464b0080f06e695d72d76fe19
sabinakanimetova5@gmail.com <sabinakanimetova5@gmail.com>
2022-12-30 23:42:03 +0600

    hw7

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import colorama
-import math,datetime
-
+import math, datetime
 # MRO
 
 class Human:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
     # def __str__(self):
     #     return f'меня зовут {self.name} и мне {self.age} лет'
 
-hum=Human('Бекболот',21)
-hum.age=45
+
+hum = Human('Бекболот', 21)
+hum.age = 45
 print(hum)
 
+abc = (f'1-возраст\n'
+       f'2-имя\n'
+       f'3-дата рождения\n'
+       f'4-машина\n'
+       f'5-ориентация\n'
+       f'6-подсчет\n'
+       f'7-выход')
+
+
 class Human2(Human):
-    def __init__(self,name,age,nasa=False):
-        super().__init__(name,age)
-        self.nasa=nasa
+    def __init__(self, name, age, nasa=False):
+        super().__init__(name, age)
+        self.nasa = nasa
+
     # def _n(self):
     #     self.nasa=True
     # def t(self):
     #     self.age *= 2
     def __orientation(self):
         print(f'{self.name} скрывает свою ориентацию')
+
     def year(self):
-        print(f'{2023-self.age}')
+        print(f'{2023 - self.age}')
 
     def car(self):
         print(f'{self.name} водит машину с 18 лет, его стаж вождения {self.age - 18}')
@@ -39,13 +49,9 @@ class Human2(Human):
     def wh(self):
         while True:
             Human2.aldik(self)
+
     def aldik(self):
-        print(f'1-возраст\n'
-              f'2-имя\n'
-              f'3-дата рождения\n'
-              f'4-машина\n'
-              f'5-ориентация\n'
-              f'6-выход')
+        print(abc)
         a = int(input('Введите число: '))
         if a == 1:
             Human2.emka(self)
@@ -57,5 +63,22 @@ class Human2(Human):
             Human2.car(self)
         elif a == 5:
             Human2.__orientation(self)
+        elif a == 6:
+            Human2.counter(self)
+        elif a == 7:
+            Human2.ostanovka(self)
+
+    def counter(self):
+        c = 0
+        for i in abc:
+            n = i.isdigit()
+            if n == True:
+                c += 1
+        print(c)
+
+    def ostanovka(self):
+        quit()
+
+
 w = Human2('Mirdjalalydin', 23)
 w.wh()
